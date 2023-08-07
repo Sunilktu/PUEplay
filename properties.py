01d9ba0cc29b9a4e0286c462b57a79d4e8569c97
Sunil <turpati.sunil@gmail.com>
2023-08-07 15:15:02 +0530

    added Tdp

--- properties.py
+++ properties.py
@@ -6,4 +6,6 @@ from CoolProp.CoolProp import PropsSI
 psychrolib.SetUnitSystem(psychrolib.SI)
 Density=CP.HumidAirProp.HAPropsSI('D','T',300,'P',101325,'R',0.1)
 WBT = psychrolib.GetTWetBulbFromHumRatio(35,0.007,101325)
-print(WBT)
+
+Tdp =psychrolib.GetTDewPointFromRelHum(35,0.4)
+print(WBT,Tdp)
